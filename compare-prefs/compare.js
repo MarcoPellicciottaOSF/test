025436b325f9ff5c104d1e6964b56f97a2f37744
MarcoPellicciotta <Marco.Pellicciotta@osf-global.com>
2020-09-16 20:17:34 -0300

    .

--- compare-prefs/compare.js
+++ compare-prefs/compare.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-var parseString = require('xml2js').parseString;
-var diffs = ['site \t pref id \t dev \t stg/dev \t stg/stg \t stg/prd'];
+var parseString = require('xml2js').parseStringPromise;
+var diffs = ['site \t pref id \t dev \t stg/dev \t comp \t stg/stg \t comp \t stg/prd \t resolve'];
 
 function escapeTsvVal(val) {
 	return (val || '').replace(/"/g, '""');
@@ -16,29 +16,40 @@ function stringifyValues(args) {
 
 
 fs.readdirSync('sites', {withFileTypes: true}).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name).forEach(dir => {
-	parseString(fs.readFileSync('sites/' + dir + '/preferences.xml'), function (err, result) {
-		parseString(fs.readFileSync('sites/' + dir + '/preferences2.xml'), function (err, result2) {
-			let customPrefs = result.preferences['custom-preferences'][0];
-			
-			customPrefs.staging[0].preference.forEach(stgPref => {
-				let devPrefDev = result2.preferences['custom-preferences'][0].development[0].preference.find(devPref => devPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
-				let devPref = customPrefs.development[0].preference.find(devPref => devPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
-				let prdPref = customPrefs.production[0].preference.find(prdPref => prdPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
-				stringifyValues([devPref, stgPref, prdPref, devPrefDev]);
-				
-				if (devPref['_'] != devPrefDev['_']) {
-					diffs.push(
-						'"' + escapeTsvVal(dir) + '"\t' +
-						'"' + escapeTsvVal(stgPref['$']['preference-id']) + '"\t' +
-						'"' + escapeTsvVal(devPrefDev['_']) + '"\t' +
-						'"' + escapeTsvVal(devPref['_']) + '"\t' +
-						'"' + escapeTsvVal(stgPref['_']) + '"\t' +
-						'"' + escapeTsvVal(prdPref['_']) + '"'
-					)
-				}
-			});
-		});
-    });
-});
+	Promise.all([parseString(fs.readFileSync('sites/' + dir + '/preferences.xml', 'utf8')), parseString(fs.readFileSync('sites/' + dir + '/preferences2.xml', 'utf8'))]).then(results => {
+		let customPrefs = results[0].preferences['custom-preferences'][0];
+		
+		customPrefs.staging[0].preference.forEach(stgPref => {
+			let devPrefDev = results[1].preferences['custom-preferences'][0].development[0].preference.find(devPref => devPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
+			let devPref = customPrefs.development[0].preference.find(devPref => devPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
+			let prdPref = customPrefs.production[0].preference.find(prdPref => prdPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
+			stringifyValues([devPref, stgPref, prdPref, devPrefDev]);
 
-fd = fs.writeFileSync('result.tsv', diffs.join("\r\n"));
+			let id = escapeTsvVal(stgPref['$']['preference-id']);
+			devPrefDev = escapeTsvVal(devPrefDev['_']);
+			devPref = escapeTsvVal(devPref['_']);
+			stgPref = escapeTsvVal(stgPref['_']);
+			prdPref = escapeTsvVal(prdPref['_']);
+			
+			if (devPref != devPrefDev) {
+				//#############    Customize by each project    #####################
+				let resolveTo = stgPref == prdPref ? stgPref : (devPrefDev || devPref);
+				//#############    Customize by each project    #####################
+				
+				diffs.push(
+					'"' + escapeTsvVal(dir) + '"\t' +
+					'"' + id + '"\t' +
+					'"' + devPrefDev + '"\t' +
+					'"' + devPref + '"\t' +
+					'"' + (devPref == stgPref) + '"\t' +
+					'"' + stgPref + '"\t' +
+					'"' + (stgPref == prdPref) + '"\t' +
+					'"' + prdPref + '"\t' +
+					'"' + '<preference preference-id=""' + id + '"">' + resolveTo + '</preference>' + '"'
+				)
+			}
+		});
+		
+		fd = fs.writeFileSync('result.tsv', diffs.join("\r\n"));
+	});
+});
