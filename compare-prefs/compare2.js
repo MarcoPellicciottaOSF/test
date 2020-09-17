dfa9b9b022f9d58ca5aedbe50197a39268ad63be
MarcoPellicciotta <Marco.Pellicciotta@osf-global.com>
2020-09-16 23:03:40 -0300

    .

--- compare-prefs/compare2.js
+++ compare-prefs/compare2.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 var parseString = require('xml2js').parseStringPromise;
-var diffs = ['site \t pref id \t stg/dev \t comp \t stg/stg \t comp \t stg/prd \t resolve'];
+var diffs = ['site \t pref id \t dev \t stg/dev \t comp \t stg/stg \t comp \t stg/prd \t resolve'];
 
 function escapeTsvVal(val) {
 	return (val || '').replace(/"/g, '""');
@@ -16,25 +16,30 @@ function stringifyValues(args) {
 
 
 fs.readdirSync('sites', {withFileTypes: true}).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name).forEach(dir => {
-	parseString(fs.readFileSync('sites/' + dir + '/preferences.xml')).then(results => {
-		let customPrefs = results.preferences['custom-preferences'][0];
+	Promise.all([parseString(fs.readFileSync('sites/' + dir + '/preferences.xml', 'utf8')), parseString(fs.readFileSync('sites/' + dir + '/preferences2.xml', 'utf8'))]).then(results => {
+		let customPrefs = results[0].preferences['custom-preferences'][0];
 		
 		customPrefs.staging[0].preference.forEach(stgPref => {
+			let devPrefDev = results[1].preferences['custom-preferences'][0].development[0].preference.find(devPref => devPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
 			let devPref = customPrefs.development[0].preference.find(devPref => devPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
 			let prdPref = customPrefs.production[0].preference.find(prdPref => prdPref['$']['preference-id'] == stgPref['$']['preference-id']) || {'_': ''};
-			stringifyValues([devPref, stgPref, prdPref]);
+			stringifyValues([devPref, stgPref, prdPref, devPrefDev]);
 
 			let id = escapeTsvVal(stgPref['$']['preference-id']);
+			devPrefDev = escapeTsvVal(devPrefDev['_']);
 			devPref = escapeTsvVal(devPref['_']);
 			stgPref = escapeTsvVal(stgPref['_']);
 			prdPref = escapeTsvVal(prdPref['_']);
 			
-			if (devPref != stgPref) {
-				let resolveTo = stgPref == prdPref ? stgPref : stgPref;
+			if (devPref != devPrefDev) {
+				//#############    Customize by each project    #####################
+				let resolveTo = stgPref == prdPref ? stgPref : (devPrefDev || devPref);
+				//#############    Customize by each project    #####################
 				
 				diffs.push(
 					'"' + escapeTsvVal(dir) + '"\t' +
 					'"' + id + '"\t' +
+					'"' + devPrefDev + '"\t' +
 					'"' + devPref + '"\t' +
 					'"' + (devPref == stgPref) + '"\t' +
 					'"' + stgPref + '"\t' +
@@ -46,7 +51,5 @@ fs.readdirSync('sites', {withFileTypes: true}).filter(dirent => dirent.isDirecto
 		});
 		
 		fd = fs.writeFileSync('result.tsv', diffs.join("\r\n"));
-	}).catch(err => {
-		console.log(err);
 	});
 });
